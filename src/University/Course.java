bb3242af9b8436ea461a1404f46dd5407d692394
SALEM .. <salemsnsalmutairi@gmail.com>
2024-11-05 08:18:09 +0300

    intitial

---
+++ src/University/Course.java
@@ -0,0 +1,46 @@
+package University;
+
+public class Course {
+    private String course;
+    private String instructor;
+    private int numStudents;
+
+    Course(String course, String instructor, int numStudents){
+this.course = course;
+this.instructor = instructor;
+this.numStudents = numStudents;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
+    public String getInstructor() {
+        return instructor;
+    }
+
+    public void setInstructor(String instructor) {
+        this.instructor = instructor;
+    }
+
+    public int getNumStudents() {
+        return numStudents;
+    }
+
+    public void setNumStudents(int numStudents) {
+        this.numStudents = numStudents;
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "course='" + course + '\'' +
+                ", instructor='" + instructor + '\'' +
+                ", numStudents=" + numStudents +
+                '}';
+    }
+}
---
+++ src/University/app.java
@@ -0,0 +1,12 @@
+package University;
+
+public class app {
+    public static void main(String[] args) {
+
+        Course course1 = new Course("math101", "dingle", 10);
+
+        System.out.println(course1.toString());
+
+
+    }
+}
